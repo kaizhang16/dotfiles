61f5222735053e2a6e0bb97e79f0de3268785838
Kai Zhang <kaizhang91@foxmail.com>
2018-04-01 17:27:46 +0800

    do not link files of other os

--- app/Main.hs
+++ app/Main.hs
@@ -9,14 +9,17 @@ import qualified Turtle      as TT
 
 main :: IO ()
 main = do
-  templatesDir <- TT.options "Deploy dotfiles to home directory" parser
-  maybeTemplates <- D.getTemplates templatesDir
-  -- print maybeTemplates
-  case maybeTemplates of
-    Just templates -> do
-      _ <- mapM_ D.link templates
-      printf (D.infoFormat "Succeed.\n")
-    Nothing -> TT.die ((T.pack . D.errorFormat) "No template found")
+  maybeOSType <- D.uname
+  case maybeOSType of
+    Nothing -> TT.die ((T.pack . D.errorFormat) "No OS type found")
+    Just osType -> do
+      templatesDir <- TT.options "Deploy dotfiles to home directory" parser
+      maybeTemplates <- D.getTemplates templatesDir
+      case maybeTemplates of
+        Just templates -> do
+          _ <- mapM_ (D.link osType) templates
+          printf (D.infoFormat "Succeed.\n")
+        Nothing -> TT.die ((T.pack . D.errorFormat) "No template found")
 
 parser :: TT.Parser TT.FilePath
 parser = TT.argPath "templates_dir" "The templates directory"
